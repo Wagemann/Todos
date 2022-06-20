9561870777e4806bda99b34ab180f35891b11dfe
Nick <wagemannnick@gmail.com>
2022-06-20 19:23:40 -0400

    Feature 13 complete

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from projects.views import ProjectListView
+from projects.views import ProjectListView, ProjectDetailView
 
 urlpatterns = [
     path("", ProjectListView.as_view(), name="projects_list"),
+    path("<int:pk>/", ProjectDetailView.as_view(), name="show_project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.decorators import login_required
 from projects.models import Project
 
@@ -10,6 +11,14 @@ from projects.models import Project
 class ProjectListView(LoginRequiredMixin, ListView):
     model = Project
     template_name = "projects/list.html"
+
+    def get_queryset(self):
+        return Project.objects.filter(members=self.request.user)
+
+
+class ProjectDetailView(LoginRequiredMixin, DetailView):
+    model = Project
+    template_name = "projects/detail.html"
 
     def get_queryset(self):
         return Project.objects.filter(members=self.request.user)
