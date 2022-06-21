51e25f290e2d502d397ee88d1a4cb49060b35570
Nick <wagemannnick@gmail.com>
2022-06-21 16:10:09 -0400

    Feature 14 complete

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.decorators import login_required
 from projects.models import Project
 
@@ -22,3 +23,15 @@ class ProjectDetailView(LoginRequiredMixin, DetailView):
 
     def get_queryset(self):
         return Project.objects.filter(members=self.request.user)
+
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    template_name = "projects/create.html"
+    fields = ["name", "description", "members"]
+
+    def form_valid(self, form):
+        item = form.save(commit=False)
+        item.members = self.request.user
+        item.save()
+        return redirect("create_project")
