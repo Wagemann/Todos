676349d12767716d61047698a635461ce1ad50ed
Nick <wagemannnick@gmail.com>
2022-06-21 16:48:08 -0400

    Feature 15 complete

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from tasks.models import Task
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    template_name = "tasks/create.html"
+    fields = ["name", "start_date", "due_date", "project", "assignee"]
+
+    def form_valid(self, form):
+        item = form.save(commit=False)
+        item.members = self.request.user
+        item.save()
+        return redirect("create_task")
